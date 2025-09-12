611ef8af4f2358e13db36dea84e6fb416bb17e79
dusk-and-dawn <david.rotnicki@gmail.com>
2025-09-12 13:43:08 +0200

    erste frage antwort fertig

--- main.py
+++ main.py
@@ -6,9 +6,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/quiz')
+@app.route('/quiz', methods=('POST','GET'))
 def quiz():
+    if request.method == 'POST':
+        correct_answers = ['Nicht-Regierungsorganisationen (NGOs)', 'Politische Stiftungen', 'temporäre Projekt-Teams']
+        answers = request.form.getlist("answers")
+        correctly_chosen = [i for i in answers if i in correct_answers]
+        if "Firmen" in answers or "Einzelpersonen" in answers or "Ausländische Regierungen" in answers:
+            return redirect(url_for('quizfail'))
+        elif len(correctly_chosen) == 3:
+            return redirect(url_for('quizsuccess'))
     return render_template('quiz1.html')
+
+@app.route('/quizfail')
+def quizfail():
+    return render_template('quizfail.html')
+
+@app.route('/quizsuccess')
+def quizsuccess():
+    return render_template('quizsuccess.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
