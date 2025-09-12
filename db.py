04b89f71f0a850fc05712bb592452bda6a6999ba
dusk-and-dawn <david.rotnicki@gmail.com>
2025-09-13 01:06:31 +0200

    added basic db functionality

--- db.py
+++ db.py
@@ -1,1 +1,23 @@
-import sqlite3
+import sqlite3 as sql
+
+def get_db(name='main.db'):
+    db = sql.connect(name)
+    create_table(db)
+    return db
+
+def create_table(db):
+    cur = db.cursor()
+    
+    cur.execute(
+        '''CREATE TABLE IF NOT EXISTS player_stats (
+        session TEXT PRIMARY KEY, 
+        score INTEGER,
+        q_answered INTEGER)'''
+        )
+    
+    db.commit()
+
+def add_to_main_db(db, session, score, q_answered):
+    cur = db.cursor()
+    cur.execute('INSERT into player_stats VALUES (?,?,?)', session, score, q_answered)
+    db.commit()
